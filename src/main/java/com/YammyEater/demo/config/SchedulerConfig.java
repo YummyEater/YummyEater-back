feb6ee4d143a712892163f18fe7e7634584a0d12
osm <osm0822@naver.com>
2025-02-12 14:19:44 +0900

    feat: 어플리케이션에 배치와 스케쥴링을 허용하도록 설정

---
+++ src/main/java/com/YammyEater/demo/config/SchedulerConfig.java
@@ -0,0 +1,13 @@
+package com.YammyEater.demo.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@ConditionalOnProperty(
+        value = "app.scheduling.enable", havingValue = "true", matchIfMissing = true
+)
+@EnableScheduling
+public class SchedulerConfig {
+}
